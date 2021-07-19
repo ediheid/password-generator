d7550b02e55442f82bf0073ead43c687c37542eb
Edith-Heidmann <eheidmann@gmail.com>
2021-07-19 16:22:41 +0200

    finished up until bonus

--- js/index.js
+++ js/index.js
@@ -1,2 +1,32 @@
 "use strict";
 
+const charset = "abcdefghijklmnopqrstuvwxyz0123456789!§$%&/()=?#,;.:-_";
+
+const output = document.querySelector(".output");
+const clickButton = document.querySelector(".button");
+
+console.log(charset);
+
+const passwordFunction = () => {
+  let passwordString = ``;
+
+  for (let i = 0; i < 20; i++) {
+    let index = Math.floor(Math.random() * charset.length);
+
+    if (i > 0 && i % 3 == 0) {
+      passwordString += charset[index].toLocaleUpperCase();
+    } else {
+      passwordString += charset[index];
+    }
+    output.textContent = passwordString;
+  }
+};
+
+// console.log(passwordFunction());
+
+clickButton.addEventListener("click", passwordFunction);
+
+// Bonus
+// =============================
+
+const defineNumber = document.querySelector(".define-number-input");
