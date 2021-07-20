c49089cf86bf67284c04fe012728315bced3bcc9
Edith-Heidmann <eheidmann@gmail.com>
2021-07-20 10:06:11 +0200

    corrected exercise as I read it incorrectly yesterday

--- js/index.js
+++ js/index.js
@@ -5,28 +5,6 @@ const charset = "abcdefghijklmnopqrstuvwxyz0123456789!§$%&/()=?#,;.:-_";
 const output = document.querySelector(".output");
 const clickButton = document.querySelector(".button");
 
-// const passwordFunction = () => {
-//   let passwordString = ``;
-
-//   for (let i = 0; i < 20; i++) {
-//     let index = Math.floor(Math.random() * charset.length);
-
-//     if (i > 0 && i % 3 == 0) {
-//       passwordString += charset[index].toLocaleUpperCase();
-//     } else {
-//       passwordString += charset[index];
-//     }
-//     output.textContent = passwordString;
-//   }
-// };
-
-// // console.log(passwordFunction());
-
-// clickButton.addEventListener("click", passwordFunction);
-
-// Bonus
-// =============================
-
 const defineNumber = document.querySelector(".define-number-input");
 
 const checkbox = document.querySelector("#checkbox");
@@ -34,29 +12,37 @@ const checkbox = document.querySelector("#checkbox");
 const definePasswordLength = () => {
   let password = ``;
 
-  // ? Checkbox for loop..
-  if (checkbox.checked == true) {
-    // alert(`Checked!`);
-    // ? input number to pass into for loop
-    let number = defineNumber.value;
+  // ? input number to pass into for loop
+  let number = defineNumber.value;
 
-    for (let i = 0; i < number; i++) {
-      let index = Math.floor(Math.random() * charset.length);
+  // ? for loop with input number
+  for (let i = 0; i < number; i++) {
+    let index = Math.floor(Math.random() * charset.length);
+
+    if (checkbox.checked == true) {
       if (i > 0 && i % 3 == 0) {
         password += charset[index].toLocaleUpperCase();
       } else {
         password += charset[index];
       }
+    } else {
+      password += charset[index];
     }
-    output.textContent = password;
 
-    // ===========
-    //? Regular length - 20 char..
-  } else {
+    output.textContent = password;
+  }
+
+  //? Default length
+  if (defineNumber.value < 1) {
     for (let i = 0; i < 20; i++) {
       let index = Math.floor(Math.random() * charset.length);
-      if (i > 0 && i % 3 == 0) {
-        password += charset[index].toLocaleUpperCase();
+
+      if (checkbox.checked == true) {
+        if (i > 0 && i % 3 == 0) {
+          password += charset[index].toLocaleUpperCase();
+        } else {
+          password += charset[index];
+        }
       } else {
         password += charset[index];
       }
